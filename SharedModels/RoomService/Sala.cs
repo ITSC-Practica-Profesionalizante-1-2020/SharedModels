2eb7fb62f37726867d8c6bd8083c68e1895ba349
u90026 <epolicardo@bancor.com.ar>
2020-07-24 22:03:14 -0300

    Seteo de Key en Id EntityBase y modificaciones el entity Sala

--- SharedModels/RoomService/Sala.cs
+++ SharedModels/RoomService/Sala.cs
@@ -6,10 +6,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 public class Sala : EntityBase
 {
-    [Key]
-    public int Id_Sala { get; set; }
+
     public string NombreSala { get; set; }
-    public DateTime FechaCreacion { get; set; }
     public DateTime? FechaBaja { get; set; }
     public DateTime? FechaUltimoUso { get; set; }
     public DateTime? TiempoAcumuladoUso { get; set; }
@@ -17,5 +15,6 @@ public class Sala : EntityBase
     public List<Participante> Participante { get; set; }
     public string PasswordSala { get; set; }
     public bool ControlAsistencia { get; set; }
+    public bool EsPersistente { get; set; }
 
 }
